0e6f9ecb8ebc5b92d236c68a7e95b74074014d71
Arsh <as3096734@gmail.com>
2023-02-01 23:07:45 +0530

    complete pagination

--- src/App.js
+++ src/App.js
@@ -5,27 +5,35 @@ function App() {
   const [products, setProducts] = useState([]); // for storing the api data
   const [pageNo, setpageNo] = useState(2);
 
+  // another type varition for adding the limit functionality
+  const [totalPages, setTotalPages] = useState(0);
+
   const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products");
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${pageNo * 10 - 10}`
+    );
     const data = await res.json(); // Nnow conver it to json
 
-    // console.log(data);
+    console.log(data);
     // Now put all the api data into 1 variable for this we use useState
 
     if (data && data.products) {
       setProducts(data.products);
+      setTotalPages(data.total / 10); // for limit functionality
+      // means total toh 100 hi rhege and 10 hi dikhane h each page pr
+      // so jidr jidr product.length use krre the udr udr hum total Page use krege
     }
   };
 
   // console.log(products);
   useEffect(() => {
     fetchProducts();
-  }, []); // jb dependncy array bhi dalege toh sirf 1st time hi fetch krege products ko
+  }, [pageNo]); // jb dependncy array bhi dalege toh sirf 1st time hi fetch krege products ko
 
   const selectedPageHandler = (selectedPage) => {
     if (
       selectedPage >= 1 &&
-      selectedPage <= products.length / 10
+      selectedPage <= totalPages
       // &&
       // selectedPage !== pageNo
     )
@@ -35,7 +43,8 @@ function App() {
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.slice(pageNo * 10 - 10, pageNo * 10).map((prod) => {
+          {/* {products.slice(pageNo * 10 - 10, pageNo * 10).map((prod) => { */}
+          {products.map((prod) => {
             return (
               <span className="products__single" key={prod.id}>
                 <img src={prod.thumbnail} alt={prod.title} />
@@ -54,7 +63,7 @@ function App() {
           <span onClick={() => selectedPageHandler(pageNo - 1)}>◀</span>
           {/* printing middle no how many number we have, suppose 100 products /10
           = 10 pages]*/}
-          {[...Array(products.length / 10)].map((_, index) => {
+          {[...Array(totalPages)].map((_, index) => {
             return (
               <span
                 className={pageNo === index + 1 ? "pagination__selected" : ""}
